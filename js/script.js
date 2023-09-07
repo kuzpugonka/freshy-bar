7abc78115b50f419153b148980eb69dd34cca8a4
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-09-07 19:36:37 +0300

    исправлены ошибки при закрытии модалок

--- js/script.js
+++ js/script.js
@@ -237,7 +237,7 @@ const calculateMakeYourOwn = () => {
   const resetForm = () => {
     makeTotalPrice.textContent = "";
     makeAddBtn.desabled = true;
-    formAdd.reset();
+    formMakeOwn.reset();
   };
 
   return { resetForm };
@@ -268,7 +268,7 @@ const calculateAdd = () => {
 
   formAdd.addEventListener("change", handlerChange);
   formControl(formAdd, () => {
-    modalAdd.closest('close')
+    modalAdd.closeModal('close');
   });
 
   const fillInForm = (data) => {
